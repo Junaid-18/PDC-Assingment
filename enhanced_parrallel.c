7ea2bfb9dd57cd551033481414d4ae44553ac07a
root <root@DESKTOP-8BL3MIG.>
2025-03-08 20:33:39 +0500

    Final code after fixing inconsistent matrix output by addressing race conditions. Stable and optimized version

--- enhanced_parrallel.c
+++ enhanced_parrallel.c
@@ -1,50 +1,63 @@
 #include <stdio.h>
 #include <limits.h>
 #include <omp.h>
+#include <string.h>
 
 #define N 300  
-#define RUNS 10  // Number of times to execute
 
-void chain_matrix(int p[], int n, int m[N][N]) { 
-    int i, j, k, cost;  
+void chain_matrix(int p[], int n, int m[N][N]) {
+    int length, i, j, k, cost;
 
-    #pragma omp parallel for private(i, j, k, cost) shared(m, p) schedule(dynamic)
-    for (int length = 2; length < n; length++) {  
-        for (i = 1; i < n - length + 1; i++) {  
+    for (length = 2; length < n; length++) {  
+        #pragma omp parallel for private(i, j, k, cost) schedule(dynamic)  
+        for (i = 1; i < (n - length + 1); i++) {  
             j = i + length - 1;
-            int min_cost = INT_MAX;//Int max will compute that  computed cost is smaller than the initial cost and store it in  min cost variable
-
+            m[i][j] = INT_MAX;
 
             for (k = i; k < j; k++) {
                 cost = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
 
-                if (cost < min_cost) {
-                    min_cost = cost;
+                if (cost < m[i][j]) {
+                    m[i][j] = cost;
                 }
             }
-            m[i][j] = min_cost;  // Update outside the parallel region
         }
     }
 }
-int main(){
-    int p[]={40,20,30,10,50};
-    int n =sizeof(p)/sizeof(p[0]);
-    int m[N][N]={0};
-    int num_of_threads=8;
-    omp_set_num_threads(num_of_threads);
-    double total_time=0.0;
-    printf("Running with %d threads \n",num_of_threads);
-    for(int i=0;i<RUNS;i++){
-        double stime=omp_get_wtime();
-        chain_matrix(p,n,m);
-        double etime=omp_get_wtime();
 
-        double exe_time=etime-stime;
-        total_time +=exe_time;
-        printf("%d iteration has exe time = %f seconds\n",i+1,exe_time);
+// Function to print the matrix
+void print_matrix(int m[N][N], int n) {
+    printf("\n Matrix:\n");
+    for (int i = 1; i < n; i++) { //this will traverse rows in matrix
+        for (int j = 1; j < n; j++) {  // this will traverse column in mmatrix
+            if (i > j) {
+                printf(" ---- ");  
+            } else {
+                printf("%5d ", m[i][j]); //for actual values displaying 
+            }
+        }
+        printf("\n");
     }
-    double average_time=total_time/RUNS;
-    printf("Average execution time for %d iterations is %.9f seconds \n",RUNS,average_time);
-    return 0;
+}
 
+int main() {
+    int p[] = {20, 30, 40, 50, 10};  
+    int n = sizeof(p) / sizeof(p[0]);  
+    int m[N][N];  
+    memset(m, 0, sizeof(m));//it will set a block of memory for a value and initial m with 0(diagonal entries)
+
+    int num_of_threads = 8;
+    omp_set_num_threads(num_of_threads);
+    
+    printf("Running with %d threads\n", num_of_threads);
+
+    double stime = omp_get_wtime();
+    chain_matrix(p, n, m);
+    double etime = omp_get_wtime();
+    
+    printf("\nParallel Chain Matrix Multiplication Execution Time: %f seconds\n", etime - stime);
+
+    print_matrix(m, n);  // Print the matrix after execution
+
+    return 0;
 }
